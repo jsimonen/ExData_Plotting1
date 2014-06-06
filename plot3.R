51451f9fccc09e08c7699754155de0e10f3f8f87
Janne Simonen <janne.simonen@gmail.com>
2014-06-06 11:33:20 +0300

    plot3 getting ready

---
+++ plot3.R
@@ -0,0 +1,44 @@
+## plot1.R
+# Coursera Exploratory Data Analysis 2014
+# Course Project 1
+# by Janne Simonen
+
+# Data Description: Measurements of electric power consumption in one household with a one-minute sampling rate over a period of almost 4 years. Different electrical quantities and some sub-metering values are available.
+# 
+# The following descriptions of the 9 variables in the dataset are taken from the UCI web site:
+#   
+#   Date: Date in format dd/mm/yyyy
+# Time: time in format hh:mm:ss
+# Global_active_power: household global minute-averaged active power (in kilowatt)
+# Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+# Voltage: minute-averaged voltage (in volt)
+# Global_intensity: household global minute-averaged current intensity (in ampere)
+# Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+# Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+# Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+
+# read the data
+datafile <- "household_power_consumption.txt"
+edata <- read.csv(datafile,stringsAsFactors=FALSE,sep = ";", na.strings = "?")
+
+# change the date column into a date object that contains the time as well
+datetime <- paste(edata[,"Date"],edata[,"Time"])          # date and time string
+datetimeobj <- strptime(datetime,format = "%d/%m/%Y %H:%M:%S")     # date object
+edata$Date <- datetimeobj
+
+# subset the data: "We will only be using data from the dates 2007-02-01 and 2007-02-02"
+# there's something funny about the definition of ">" for dates - it works like ">="
+edata2 <- subset(edata,Date > strptime("2007-02-01",format = "%Y-%m-%d"))
+edata2 <- subset(edata2,Date < strptime("2007-02-03",format = "%Y-%m-%d"))
+
+# make the line plot
+# note that the x-axis abbreviated days are in Finnish, my locale!
+with(edata2,plot(Date,Sub_metering_1,type="l",col="black",xlab="",ylab="Energy sub metering"))
+with(edata2,lines(Date,Sub_metering_2,type="l",col="red"))
+with(edata2,lines(Date,Sub_metering_3,type="l",col="blue"))
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+# save the plot to a png file
+dev.copy(png, file = "plot3.png") ## Copy my plot to a PNG file
+dev.off() ## Don't forget to close the PNG device!
+
